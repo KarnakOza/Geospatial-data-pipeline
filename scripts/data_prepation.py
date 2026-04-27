e0e8164344a45d963f96cfd3ff6d2291bcfb7c50
Fermy <123935594+KarnakOza@users.noreply.github.com>
2026-04-27 23:34:57 +0530

    Implement Sentinel-1 GRDH scene search
    Added functionality to search for Sentinel-1 GRDH scenes within a specified area of interest (AOI) and print the results.

--- scripts/data_prepation.py
+++ scripts/data_prepation.py
@@ -3,3 +3,27 @@ import geopandas as gpd
 gdf = gpd.read_file("data/raw/data.geojson")
 gdf = gdf.dropna(subset=["geometry"])
 gdf = gdf.to_crs(epsg=4326)
+
+
+import asf_search as asf
+
+AOI = 'POLYGON((69.6475 22.7016,69.7499 22.7016,69.7499 22.7728,69.6475 22.7728,69.6475 22.7016))'
+
+results = asf.search(
+    platform=asf.PLATFORM.SENTINEL1,
+    beamMode='IW',
+    intersectsWith=AOI,
+    start='2025-04-01',
+    end='2026-04-08'
+)
+
+# keep ONLY GRDH
+grdh = [r for r in results if "GRDH" in r.properties['sceneName']]
+
+print("Number of GRDH scenes:", len(grdh))
+
+scene_names = list(set([r.properties["sceneName"] for r in grdh]))
+print(len(scene_names))
+
+for r in grdh:
+    print(r.properties['sceneName'])
